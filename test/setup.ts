367f06cd09e403bdfef74d46252710693e6b6be8
nguyenbn7 <148407637+nguyenbn7@users.noreply.github.com>
2025-03-29 13:16:57 +0700

    update

--- test/setup.ts
+++ test/setup.ts
@@ -1,5 +1,5 @@
-import { execSync } from "child_process";
-import { GenericContainer, StartedTestContainer } from "testcontainers";
+import { execSync } from "node:child_process";
+import { GenericContainer, type StartedTestContainer } from "testcontainers";
 
 const TEST_DATABASE = "blog_test_db";
 const TEST_PASSWORD = "test";
@@ -11,25 +11,35 @@ const globalThisExtended = globalThis as typeof globalThis & {
 };
 
 export async function setup() {
-  const container = await new GenericContainer("postgres:17-alpine")
-    .withEnvironment({ POSTGRES_DB: TEST_DATABASE })
-    .withEnvironment({ POSTGRES_PASSWORD: TEST_PASSWORD })
-    .withExposedPorts({ container: 5432, host: PORT })
-    .start();
+  try {
+    console.log("Starting postgres docker");
+    const container = await new GenericContainer("postgres:17-alpine")
+      .withEnvironment({ POSTGRES_DB: TEST_DATABASE })
+      .withEnvironment({ POSTGRES_PASSWORD: TEST_PASSWORD })
+      .withExposedPorts({ container: 5432, host: PORT })
+      .start();
 
-  if (!globalThisExtended.container) {
-    globalThisExtended.container = container;
+    if (!globalThisExtended.container) {
+      globalThisExtended.container = container;
 
-    execSync("bun run db:push", {
-      env: {
-        DATABASE_URL,
-      },
-    });
+      execSync("bun run db:push", {
+        env: {
+          DATABASE_URL,
+        },
+      });
+    }
+
+    console.log("Done setup");
+  } catch (error) {
+    console.log("Something went wrong:", error);
+    throw error;
   }
 }
 
 export async function teardown() {
-  if (globalThisExtended.container) {
-    await globalThisExtended.container.stop();
-  }
+  try {
+    if (globalThisExtended.container) {
+      await globalThisExtended.container.stop();
+    }
+  } catch {}
 }
--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   test: {
     passWithNoTests: true,
     globals: true,
-    include: ["./test/**.test.ts"],
+    include: ["./test/**/*.test.ts"],
     env: loadEnv("testing", process.cwd(), ""),
     globalSetup: "./test/setup.ts",
   },
